905da8092de12ba19bfb290f81f7e6fa81ecebd8
Andreas Appelqvist <andreas.appelqvist@gmail.com>
2016-09-09 10:13:44 +0200

    init 10878

---
+++ UVa_10878/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Andréas Appelqvist on 09/09/16.
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Init");
+    }
+}
