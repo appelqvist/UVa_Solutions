f1e844b5ae0cfb6c74d08e06f6468878a4c88f17
Andréas <andreas.appelqvist@gmail.com>
2016-09-18 13:35:00 +0200

    Init

---
+++ UVa_839/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by Andréas Appelqvist on 2016-09-18.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Init");
+    }
+}
