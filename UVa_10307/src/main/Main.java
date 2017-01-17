8f0fa7eba7c8887890792dc6b17641e5f26b87d5
Andréas <andreas.appelqvist@gmail.com>
2017-01-17 17:31:06 +0100

    init 10307

---
+++ UVa_10307/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * Created by Andréas Appelqvist on 2017-01-17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main(){
+
+    }
+
+}
